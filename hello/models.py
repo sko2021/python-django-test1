678b33f021f9277f4b7487afef6f43de7cdd92f4
sko2021 <86480534+sko2021@users.noreply.github.com>
2021-06-28 10:24:02 -0400

    Add files via upload

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    
+
+# Create your models here.
+
+class UspsServices:
+    id : int
+    serviceName : str    
+    serviceDescription : str    
+    accessFlag : bool
+    
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import UspsServices
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from xml.dom.minidom import parse,parseString,Document
+import os
 def hello(request):
-   return render(request, 'bpgtemplate.html')
+
+   #These objects will be fetched from the XML File
+ xmldoc = ET.parse('/Users/chirasvi/Django/myproject/myapp/bpg.xml')
+ root = xmldoc.getroot()
+ service = UspsServices()
+ serviceList = []
+ for child in root:
+    # print(child.tag,child.attrib)
+     print(child.attrib['serviceName'])
+     serviceList.extend
+     service.serviceName = child.attrib['serviceName']
+     service.serviceDescription = child.attrib['serviceDescription']
+     service.accessFlag = child.attrib['accessFlag']
+     service.id = child.attrib['id']
+     serviceList[int(service.id)-1]=service
+
+ print(serviceList[0].serviceName)
+ print(serviceList[1].serviceName)
+ return render(request, 'bpgtemplate.html',{"serviceList":serviceList})
