e24d3972b4a7cd131baf9bff1363bdfb9ffefa5d
sko2021 <86480534+sko2021@users.noreply.github.com>
2021-06-28 10:02:29 -0400

    Add files via upload

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class UspsServices:
-    id : int
-    serviceName : str    
-    serviceDescription : str    
-    accessFlag : bool
-    
--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import UspsServices
-import xml.etree.ElementTree as ET
 
 def hello(request):
-
-   #These objects will be fetched from the XML File
- xmldoc = minidom.parse('bpg.xml')
-    root = xmldoc.getroot()
-    serviceList = []
-    for service in root.findall('service'):
-        # get the service details
-        serviceList.append(service)
-        return render(request, 'bpgtemplate.html',{"serviceList":serviceList})
+   return render(request, 'bpgtemplate.html')
