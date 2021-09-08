ad52569c303d73a60cb737901ce00e13fcaf97f2
sko2021 <86480534+sko2021@users.noreply.github.com>
2021-09-07 21:36:44 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import UspsServices
-import xml.etree.ElementTree as ET
 
 def hello(request):
-
-   #These objects will be fetched from the XML File
- xmldoc = minidom.parse('bpg.xml')
-    root = xmldoc.getroot()
-    serviceList = []
-    for service in root.findall('service'):
-        # get the service details
-        serviceList.append(service)
-        return render(request, 'bpgtemplate.html',{"serviceList":serviceList})
-   return render(request, 'bpgtemplate.html')
+   serviceList = UspsServices.objects.all() 
+   return render(request, 'bpgtemplate.html',{"serviceList":serviceList})
